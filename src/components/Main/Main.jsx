14b248bbf732b417b1955dc6dad5ec428076fbb9
erbolken <msuharik2@gmail.com>
2024-08-02 17:51:15 +0600

    delete ts.config, typescript fully deleted | install react-modal

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './main.scss'
 import Logo from '../images/Logo.svg'
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import WalletIcon from '@mui/icons-material/Wallet';
 import CheckroomIcon from '@mui/icons-material/Checkroom';
 import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import Modal from 'react-modal'
 
 const Main = () => {
+    const [modalIsOpen, setIsOpen] = useState(false)
+
+    const openModal = () => {
+        setIsOpen(true)
+    }
+
+    const closeModal = () => {
+        setIsOpen(false)
+    }
+
+
     return (
         <div className='wrapper'>
             <div className='sidebar'>
@@ -21,8 +33,14 @@ const Main = () => {
             <div className='main'>
 
             <p>Главная страница</p>
-            <p>Добавить нового сотрудника</p>
+            <p onClick={openModal}>Добавить нового сотрудника</p>
             </div>
+            <Modal
+            isOpen={modalIsOpen}
+            onReq
+            >
+
+            </Modal>
         </div>
     );
 };
